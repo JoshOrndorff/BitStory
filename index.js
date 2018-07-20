ed777f2695203efa99d6fcda03948aadbdb62099
Joshy Orndorff <admin@joshorndorff.com>
2018-07-19 20:47:34 -0400

    Significant js cleanup. Parent is case insensative, auto-mine doesn't update DOM each time, better abstraction.

--- index.js
+++ index.js
@@ -25,46 +25,41 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   // Update the hash whenever the text changes
-  parentBox.addEventListener("input", updateHash)
-  wordBox  .addEventListener("input", updateHash)
-  nonceBox .addEventListener("input", updateHash)
+  parentBox.addEventListener("input", hashFromDOM)
+  wordBox  .addEventListener("input", hashFromDOM)
+  nonceBox .addEventListener("input", hashFromDOM)
 
-  // Handle clicking the increment buttons
-  incButton.addEventListener("click", () => {changeBy( 1);updateHash()})
-  decButton.addEventListener("click", () => {changeBy(-1);updateHash()})
-
-  // Auto-mine when the secret button is clicked
-  autoButton.addEventListener("click", () => {
-
-    // Variable difficulty for auto-mine
-    var diff = 3
-
-    var hash = ""
-    var oldHash
-    while(hash !== oldHash && hash.slice(0, diff) !== "0".repeat(diff)) {
-      var nonce = parseInt(nonceBox.value, 10)
-      changeBy(1)
-
-      oldHash = hash
-      hash = getHash()
-    }
-    updateHash()
-  })
+  // Handle clicking the buttons
+  incButton.addEventListener("click", () => {changeBy( 1)})
+  decButton.addEventListener("click", () => {changeBy(-1)})
+  randomButton.addEventListener("click", genRandom)
+  autoButton.addEventListener("click", autoMine)
 
 
 
   /**
-   * Get the hash and update the DOM
-   * Also checks for the secret keyword
+   * Automatically mines until a block with 3 leading 0's is found
    */
-  function updateHash(){
-    if (parentBox.value === "auto-mine") {
-      autoDiv.style.display = "inline"
+  function autoMine() {
+    // Variable difficulty for auto-mine
+    var diff = 3
+
+    var parent = getParent()
+    var word = getWord()
+    var nonce = getNonce()
+    var oldHash
+    for (var hash = ""; hash !== oldHash && hash.slice(0, diff) !== "0".repeat(diff); nonce++) {
+
+      oldHash = hash
+      hash = getHash(parent, word, nonce)
     }
-    hashDiv.innerHTML = getHash()
+    nonceBox.value = --nonce
+
+    hashFromDOM()
   }
 
-  //TODO Why is this a function?
+
+
   /**
    * Change the nonce in the DOM by n
    * @param n The amount to change by
@@ -73,36 +68,96 @@ window.addEventListener("DOMContentLoaded", () => {
     var nonce = parseInt(nonceBox.value, 10)
     nonce += n
     nonceBox.value = nonce
+    hashFromDOM()
   }
 
-  // Handle clicking the random button
-  randomButton.addEventListener("click", () => {
 
-    // Generate the random 32-bit nonce
+
+  /**
+   * Generates a random 32-bit nonce, puts it in the DOM, and updates hashes.
+   */
+  function genRandom() {
     var nonce = new Uint32Array(1)
     window.crypto.getRandomValues(nonce)
     nonceBox.value = nonce[0]
 
-    updateHash()
-  })
+    hashFromDOM()
+  }
+
+
+
+  /**
+   * Reads in and validates, values from the DOM.
+   * Enables auto-mine feature on secret code input
+   */
+  function hashFromDOM() {
+    // Check for enabling auto-mine
+    if (parentBox.value === "auto-mine") {
+      autoDiv.style.display = "inline"
+    }
+
+    hashDiv.innerHTML = getHash(getParent(), getWord(), getNonce())
+  }
+
+
+
+  /**
+   * Reads in and validates the parent hash from the DOM.
+   * Treats parent case-insensatively because it most likely represents a hexadecimal number.
+   * @return The validated parent hash
+   */
+  function getParent() {
+    var parent = parentBox.value.toLowerCase()
+    // No required validation yet
+    return parent
+  }
+
+
+
+  /**
+   * Reads in and validates the word from the DOM.
+   * @return The validated word
+   */
+  function getWord() {
+    var word = wordBox.value
+
+    // Validate word: No spaces
+    if (word.indexOf(" ") !== -1) {
+      word = word.replace(" ", "");
+      console.warn("Invalid word. Using " +  + " instead.")
+    }
+
+    return word
+  }
+
+
+
+  /**
+   * Reads in and validates the nonce from the DOM.
+   * @return The validated nonce
+   */
+  function getNonce() {
+    var nonce = parseInt(nonceBox.value, 10)
+
+    // Validation nonce: Must be a number
+    if (isNaN(nonce)) {
+      nonce = 0
+      console.warn("Invalid nonce. Using 0 instead.")
+    }
+
+    return nonce
+  }
+
 
   /**
    * Calculate the sha256 hash of the parameters, and return the first
    * 8 hexadigits as a string
-   * @return The computed has value
+   * @param parent
+   * @param word
+   * @param nonce
+   * @return the hash
    */
-  function getHash() {
-    // Read in the values
-    var nonce = parseInt(nonceBox.value, 10)
-    var parent = parentBox.value
-    var word = wordBox.value
-
-    // Validate nonce
-    //TODO Where should validation happen and what should result when it fails.
-    if (isNaN(nonce)) {
-      nonce = 0
-    }
-
+  function getHash(parent, word, nonce) {
     var concat = parent + word + nonce
     var hash = sha256(concat).slice(0, 8)
 
